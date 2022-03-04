cb59d15608a4913cffc8dbc015fa9d400d38ac45
vascoventura <vasco.acv@hotmail.com>
2022-03-04 00:25:02 -0800

    Main Menu Design Complete

--- app/src/main/java/com/example/popcornandsodaonline/Login.java
+++ app/src/main/java/com/example/popcornandsodaonline/Login.java
@@ -32,59 +32,51 @@ public class Login extends AppCompatActivity {
         progressBar = findViewById(R.id.progress);
         buttonLogin = findViewById(R.id.buttonLogin);
 
-        textViewLoginMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SignUp.class);
-                startActivity(intent);
-                finish();
-            }
+        textViewLoginMessage.setOnClickListener(view -> {
+            //Intent intent = new Intent(getApplicationContext(), SignUp.class);
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username, password;
-                username = String.valueOf(textInputUsername.getText());
-                password = String.valueOf(textInputPassword.getText());
+        buttonLogin.setOnClickListener(view -> {
+            String username, password;
+            username = String.valueOf(textInputUsername.getText());
+            password = String.valueOf(textInputPassword.getText());
 
-                if(!username.equals("") && !password.equals("")) {
-                    progressBar.setVisibility(View.VISIBLE);
-                    Handler handler = new Handler();
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            //Starting Write and Read data with URL
-                            //Creating array for parameters
-                            String[] field = new String[2];
-                            field[0] = "username";
-                            field[1] = "password";
-                            //Creating array for data
-                            String[] data = new String[2];
-                            data[0] = username;
-                            data[1] = password;
+            if(!username.equals("") && !password.equals("")) {
+                progressBar.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.post(() -> {
+                    //Starting Write and Read data with URL
+                    //Creating array for parameters
+                    String[] field = new String[2];
+                    field[0] = "username";
+                    field[1] = "password";
+                    //Creating array for data
+                    String[] data = new String[2];
+                    data[0] = username;
+                    data[1] = password;
 
-                            PutData putData = new PutData("http://192.168.85.37/loginRegister/login.php", "POST", field, data);
-                            if (putData.startPut()) {
-                                if (putData.onComplete()) {
-                                    progressBar.setVisibility(View.GONE);
-                                    String result = putData.getResult();
-                                    if(result.equals("Login Success")){
-                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
-                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }else{
-                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
-                                    }
-                                }
+                    PutData putData = new PutData("http://192.168.85.37/loginRegister/login.php", "POST", field, data);
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            progressBar.setVisibility(View.GONE);
+                            String result = putData.getResult();
+                            if(result.equals("Login Success")){
+                                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }else{
+                                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
                             }
-                            //End Write and Read data with URL
                         }
-                    });
-                } else{
-                    Toast.makeText(getApplicationContext(), "All Fields Are Required!", Toast.LENGTH_LONG).show();
-                }
+                    }
+                    //End Write and Read data with URL
+                });
+            } else{
+                Toast.makeText(getApplicationContext(), "All Fields Are Required!", Toast.LENGTH_LONG).show();
             }
         });
 
